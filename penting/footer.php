ff9654824a882b89d0382f5b945e25f174b30a53
kmfreyzal <mappingradar@gmail.com>
2025-11-21 02:30:12 +0700

    menambahkan footer dan css #kafi

---
+++ penting/footer.php
@@ -0,0 +1,29 @@
+<!-- Panggil file CSS eksternal -->
+<link rel="stylesheet" href="assets/css/footer_style.css">
+
+<footer>
+  <div class="footer-top">
+    <div class="footer-container">
+      <div class="footer-section">
+        <h6>Tentang Kami</h6>
+        <ul>
+          <li><a href="page.php?type=aboutus">Tentang Kami</a></li>
+          <li><a href="page.php?type=faqs">FAQs</a></li>
+          <li><a href="page.php?type=privacy">Privacy</a></li>
+          <li><a href="admin/">Admin Login</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-section">
+        <h6>Connect With Us</h6>
+        <ul class="social-links">
+          <li><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
